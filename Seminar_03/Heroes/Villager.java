e88cc32c89665745e4d77ca75156705bba7516cc
Alina Lavrik <alina_voro@mail.ru>
2023-02-17 22:36:54 +0300

    Add task from Seminar_03

---
+++ Seminar_03/Heroes/Villager.java
@@ -0,0 +1,16 @@
+package Seminar_03.Heroes;
+
+public class Villager extends Warrior {
+  
+
+    public Villager() {
+        super("Villager__", "Green", 1, 1, 1, 0, 1, 1, 1);
+       
+    }
+
+
+    /**Переопределить getInfo так, чтобы он возвращал строки:"Я крестьянин", "Я снайпер"  */
+    @Override
+    public String getInfo() {return "Я Крестьянин ";}
+
+}
