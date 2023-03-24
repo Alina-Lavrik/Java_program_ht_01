8d909dc809b6aacba37f2263625ffa860a1c64a3
Alina Lavrik <alina_voro@mail.ru>
2023-03-24 17:17:15 +0300

    Add JAVA OOP Lesson_04_05

---
+++ Lesson_04_05/GenericTest.java
@@ -0,0 +1,10 @@
+package Lesson_04_05;
+
+public class GenericTest {
+    public static void main(String[] args) {
+        // T type
+        // E element
+        // K key
+        // V value
+    }
+}
